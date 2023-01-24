74ed0c525a16c01d0c43a400912047e5e90893b8
Ren Besson <ren.besson@outlook.com>
2023-01-23 22:44:08 -0400

    Express.js Added

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => res.send("Navigate to /send or /routes"));
+
+app.get("/send", (req, res) => res.sendFile(path.join(__dirname, "public/send.html")));
+
+app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`));
