7195644fd05692e995842ae9422b79ad6f375bbb
Ren Besson <ren.besson@outlook.com>
2023-01-25 19:51:09 -0400

    Adding Routes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 
+const db = require("./db/db.json");
+
 const app = express();
-const PORT = 80;
+const PORT = 5000;
 
 app.use(express.static("public"));
 
@@ -10,4 +12,19 @@ app.get("/", (req, res) => res.send("Navigate to /send or /routes"));
 
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
 
-app.listen(process.env.PORT || 5000);
+app.get("/api/notes", (req, res) => {
+  res.json(db);
+});
+
+app.post("/api/notes", (req, res) => {
+  res.json(req.body);
+  console.log(req.method);
+
+  if (req.body) {
+    console.log(req.body);
+  }
+});
+
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
+
+app.listen(process.env.PORT || PORT);
